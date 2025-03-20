5b680bcdd659399902ad81d64f1ddb9c539f833b
Vedant Mukherjee <31290566+vedantm8@users.noreply.github.com>
2025-03-20 19:35:34 -0400

    added index.js for routing

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
-//An index file that returns a function that attaches all your routes to your app
-//Lecture Code Refernece -> https://github.com/stevens-cs546-cs554/CS-546/blob/master/lecture_05/routes/index.js
-
-// TODO: Update Routes
-// import postRoutes from './people.js';
-// import userRoutes from './stocks.js';
+import postRoutes from './posts.js';
+import userRoutes from './users.js';
+import commentRoutes from "./comments.js"
 
 const constructorMethod = (app) => {
-//   app.use('/people', postRoutes);
-//   app.use('/stocks', userRoutes);
+    app.use('/posts', postRoutes);
+    app.use('/users', userRoutes);
+    app.use('/comments', commentRoutes)
 
-  app.use('*', (req, res) => {
-    return res.status(404).json({error: 'Not found'});
-  });
+    app.use('*', (req, res) => {
+        return res.status(404).json({ error: 'Not found' });
+    });
 };
 
 export default constructorMethod;
