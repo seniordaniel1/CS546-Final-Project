5278d6882460fca9c1d44cf4e4ebec1a2eaba914
Vedant Mukherjee <31290566+vedantm8@users.noreply.github.com>
2025-03-21 14:03:15 -0400

    added framework for post data functions

---
+++ data/posts.js
@@ -0,0 +1,23 @@
+import { posts } from "../config/mongoCollections.js";
+
+const exportedMethods = {
+    // TODO: Determine what arguments are needed to create a new post
+    // TODO: Populate function to add a post to post Collection 
+    createPost: async () => {
+    },
+    // TODO: Populate function to get all posts from post collection
+    getAllPosts: async () => {
+    },
+    // TODO: Populate function to get post by ID 
+    getPostById: async (postId) => {
+    },
+    // TODO: Populate function to remove post by Id
+    removeUser: async (postId) => {
+    },
+    // TODO: Determine what arguments are needed to update an existing post
+    // TODO: Populate function to update post
+    updateUser: async () => {
+    }
+}
+
+export default exportedMethods;
